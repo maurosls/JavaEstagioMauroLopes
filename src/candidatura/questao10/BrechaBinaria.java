cf15b0511d91f210402e237cdf80b427de29b611
mauro <maurojunior@id.uff.br>
2017-07-18 15:19:20 -0300

    questão 10

---
+++ src/candidatura/questao10/BrechaBinaria.java
@@ -0,0 +1,40 @@
+
+package candidatura.questao10;
+
+/**
+ *
+ * @author Mauro
+ */
+public class BrechaBinaria {
+    
+    public BrechaBinaria(){}
+
+
+    public int solucao(int N){
+        
+        if(N<1 || N >2147483647){
+            return 0;
+        }
+        
+        String bin = transformaBinario(N);
+        //System.out.println(bin);
+        String[] intervalos = bin.split("1");
+        
+        int cand = 0;
+        for (int i = 0; i < intervalos.length; i++) {
+            //System.out.println(intervalos[i]);
+            if (intervalos[i].length() > intervalos[cand].length()) {
+                cand = i;
+            }
+        }
+        System.out.println("O número "+N+" em binário é igual a "+bin);
+        System.out.println("A Brecha Binária deste número é: "+intervalos[cand]);
+        
+        return intervalos[cand].length();
+    }
+
+    private String transformaBinario(int N) {
+        String bin = Integer.toBinaryString(N);
+        return bin;
+    }  
+}
---
+++ src/candidatura/questao10/Principal.java
@@ -0,0 +1,20 @@
+
+package candidatura.questao10;
+
+/**
+ *
+ * @author Mauro
+ */
+public class Principal {
+    public static void main(String[] args) {
+    
+        
+        int num = 4587812;
+    
+        BrechaBinaria bb = new BrechaBinaria();
+        int resp = bb.solucao(num);
+    
+    }
+
+    
+}
